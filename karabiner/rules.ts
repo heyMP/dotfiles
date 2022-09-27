deb3475906f01e13182f0885777237058f66fe5a
heyMP <mgp140@gmail.com>
2022-09-27 12:27:56 -0400

    fork karabiner-ts project and add custom config

---
+++ karabiner/rules.ts
@@ -0,0 +1,341 @@
+import fs from "fs";
+import { KarabinerRules } from "./types";
+import { createHyperSubLayers, app, open } from "./utils";
+
+const rules: KarabinerRules[] = [
+  // Define the Hyper key itself
+  {
+    description: "Hyper Key (⌃⌥⇧⌘)",
+    manipulators: [
+      {
+        description: "Caps Lock -> Hyper Key",
+        from: {
+          key_code: "caps_lock",
+        },
+        to: [
+          {
+            key_code: "left_shift",
+            modifiers: ["left_command", "left_control", "left_option"],
+          },
+        ],
+        to_if_alone: [
+          {
+            key_code: "escape",
+          },
+        ],
+        type: "basic",
+      },
+      // {
+      //   type: "basic",
+      //   description: "Disable CMD + Tab to force Hyper Key usage",
+      //   from: {
+      //     key_code: "tab",
+      //     modifiers: {
+      //       mandatory: ["left_command"],
+      //     },
+      //   },
+      //   to: [
+      //     {
+      //       key_code: "tab",
+      //     },
+      //   ],
+      // },
+      {
+        type: "basic",
+        description: "Slash -> Hyper Key",
+        from: {
+          key_code: "slash",
+        },
+        to: [
+          {
+            key_code: "left_shift",
+            modifiers: ["left_command", "left_control", "left_option"],
+          },
+        ],
+        to_if_alone: [
+          {
+            key_code: "slash",
+          },
+        ],
+      },
+      // heyMP Changes
+      {
+        description: "Change right_command+hjkl to arrow keys",
+        from: {
+          key_code: "h",
+          modifiers: {
+            mandatory: ["right_command"],
+            optional: ["any"],
+          },
+        },
+        to: [
+          {
+            key_code: "left_arrow",
+          },
+        ],
+        type: "basic",
+      },
+      {
+        from: {
+          key_code: "j",
+          modifiers: {
+            mandatory: ["right_command"],
+            optional: ["any"],
+          },
+        },
+        to: [
+          {
+            key_code: "down_arrow",
+          },
+        ],
+        type: "basic",
+      },
+      {
+        from: {
+          key_code: "k",
+          modifiers: {
+            mandatory: ["right_command"],
+            optional: ["any"],
+          },
+        },
+        to: [
+          {
+            key_code: "up_arrow",
+          },
+        ],
+        type: "basic",
+      },
+      {
+        from: {
+          key_code: "l",
+          modifiers: {
+            mandatory: ["right_command"],
+            optional: ["any"],
+          },
+        },
+        to: [
+          {
+            key_code: "right_arrow",
+          },
+        ],
+        type: "basic",
+      },
+      {
+        type: "basic",
+        description: "Escape -> grave_accent_and_tilde (`)",
+        from: {
+          key_code: "escape",
+        },
+        to: [
+          {
+            key_code: "grave_accent_and_tilde",
+            modifiers: ["left_command", "left_control", "left_option"],
+          },
+        ],
+        // leave blank so we don't get double keys
+        to_if_alone: [
+          {
+            key_code: "grave_accent_and_tilde",
+          },
+        ],
+      },
+    ],
+  },
+  ...createHyperSubLayers({
+    // o = "Open" applications
+    o: {
+      g: app("Google Chrome"),
+      b: app("Brave"),
+      f: app("Firefox"),
+      c: app("Visual Studio Code"),
+      d: app("Discord"),
+      s: app("Slack"),
+      t: app("iTerm"),
+    },
+
+    // w = "Window" via rectangle.app
+    w: {
+      h: {
+        description: "Window: First Third",
+        to: [
+          {
+            key_code: "d",
+            modifiers: ["right_option", "right_control"],
+          },
+        ],
+      },
+      l: {
+        description: "Window: Last Third",
+        to: [
+          {
+            key_code: "g",
+            modifiers: ["right_option", "right_control"],
+          },
+        ],
+      },
+      t: {
+        description: "Window: Last Two Thirds",
+        to: [
+          {
+            key_code: "t",
+            modifiers: ["right_option", "right_control"],
+          },
+        ],
+      },
+      y: {
+        description: "Window: Left Half",
+        to: [
+          {
+            key_code: "left_arrow",
+            modifiers: ["right_option", "right_control"],
+          },
+        ],
+      },
+      o: {
+        description: "Window: Right Half",
+        to: [
+          {
+            key_code: "right_arrow",
+            modifiers: ["right_option", "left_command"],
+          },
+        ],
+      },
+      f: {
+        description: "Window: Full Screen",
+        to: [
+          {
+            key_code: "keypad_enter",
+            modifiers: ["right_option", "left_command"],
+          },
+        ],
+      },
+      u: {
+        description: "Window: Previous Tab",
+        to: [
+          {
+            key_code: "tab",
+            modifiers: ["right_control", "right_shift"],
+          },
+        ],
+      },
+      i: {
+        description: "Window: Next Tab",
+        to: [
+          {
+            key_code: "tab",
+            modifiers: ["right_control"],
+          },
+        ],
+      },
+    },
+
+    // s = "System"
+    s: {
+      u: {
+        to: [
+          {
+            key_code: "volume_increment",
+          },
+        ],
+      },
+      j: {
+        to: [
+          {
+            key_code: "volume_decrement",
+          },
+        ],
+      },
+      i: {
+        to: [
+          {
+            key_code: "display_brightness_increment",
+          },
+        ],
+      },
+      k: {
+        to: [
+          {
+            key_code: "display_brightness_decrement",
+          },
+        ],
+      },
+      l: {
+        to: [
+          {
+            key_code: "q",
+            modifiers: ["right_control", "left_command"],
+          },
+        ],
+      },
+      p: {
+        to: [
+          {
+            key_code: "play_or_pause",
+          },
+        ],
+      },
+      semicolon: {
+        to: [
+          {
+            key_code: "fastforward",
+          },
+        ],
+      },
+      e: {
+        to: [
+          {
+            // Emoji picker
+            key_code: "spacebar",
+            modifiers: ["right_control", "left_command"],
+          },
+        ],
+      },
+    },
+
+    // v = "moVe" which isn't "m" because we want it to be on the left hand
+    // so that hjkl work like they do in vim
+    v: {
+      h: {
+        to: [{ key_code: "left_arrow" }],
+      },
+      j: {
+        to: [{ key_code: "down_arrow" }],
+      },
+      k: {
+        to: [{ key_code: "up_arrow" }],
+      },
+      l: {
+        to: [{ key_code: "right_arrow" }],
+      },
+      // Magicmove via homerow.app
+      m: {
+        to: [{ key_code: "f", modifiers: ["right_control"] }],
+      },
+      // Scroll mode via homerow.app
+      s: {
+        to: [{ key_code: "j", modifiers: ["right_control"] }],
+      },
+    },
+  }),
+];
+
+fs.writeFileSync(
+  "karabiner.json",
+  JSON.stringify(
+    {
+      global: {
+        show_in_menu_bar: false,
+      },
+      profiles: [
+        {
+          name: "Default",
+          complex_modifications: {
+            rules,
+          },
+        },
+      ],
+    },
+    null,
+    2
+  )
+);
