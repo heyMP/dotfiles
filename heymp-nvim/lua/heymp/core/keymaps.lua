d9b7f180aa0af3f3bf506cab32414ae2088839de
Michael Potter <mgp140@gmail.com>
2022-11-25 20:26:34 -0500

    feat: added oldfiles to telescope

--- heymp-nvim/lua/heymp/core/keymaps.lua
+++ heymp-nvim/lua/heymp/core/keymaps.lua
@@ -60,6 +60,7 @@ keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
 keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+keymap("n", "<leader>fr", ":Telescope oldfiles<CR>", opts)
 
 -- Git
 keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
