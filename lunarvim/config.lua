c77c7e1f8591721c95dedd957509765ab4256a40
Michael Potter <mpotter@redhat.com>
2024-02-23 10:19:26 -0500

    do not format ts files automatically

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -14,7 +14,7 @@ vim.opt.foldenable = false --[[ don't expand by default ]]
 lvim.log.level = 'info'
 lvim.format_on_save = {
   enabled = true,
-  pattern = { '*.lua', "*.ts" },
+  pattern = { '*.lua' },
   timeout = 1000,
 }
 -- to disable icons and use a minimalist setup, uncomment the following
