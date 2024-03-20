6f70a43cd88cf9577ab1853e824b4749ab827917
Michael Potter <mpotter@redhat.com>
2024-03-20 18:33:58 -0400

    add top level keymaps

--- kickstart.nvim/init.lua
+++ kickstart.nvim/init.lua
@@ -310,6 +310,9 @@ require('lazy').setup({
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        ['<leader>f'] = { name = '[F]iles', _ = 'which_key_ignore' },
+        ['<leader>x'] = { name = 'Gitsigns', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
       }
     end,
   },
